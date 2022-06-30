6a74615aa32a1da3572a36fa6d66c2f7bcdfadd6
= <=>
2022-06-30 11:54:04 -0400

    add error msg and styling tweaks

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import countryCodes from "./countryCodes.js";
 import stateCodes from "./stateCodes.js";
 
 function App() {
+  const APIKey = "7841aaead8245e0fde0256620686a3e1";
   const [location, setLocation] = useState("");
-  const [locationName, setLocationName] = useState("");
+  const [cityName, setCityName] = useState("");
   const [countryCode, setCountryCode] = useState();
+  const [countryName, setCountryName] = useState();
   const [stateCode, setStateCode] = useState();
   const [stateName, setStateName] = useState();
   const [currentTemp, setCurrentTemp] = useState();
-  const [lat, setLat] = useState(null);
-  const [lon, setLon] = useState(null);
+  // const [lat, setLat] = useState(null);
+  // const [lon, setLon] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [wind, setWind] = useState(null);
   const [feelsLike, setFeelsLike] = useState(null);
   const [weather, setWeather] = useState(null);
   const [description, setDescription] = useState(null);
   const [pollutionAQI, setPollutionAQI] = useState(null);
-  // check if a zip code was entered. If so, use the 'zip' param.
-  // otherwise use 'q'
+  const [errorMsg, setErrorMsg] = useState("");
+
+  // check if a US zip code was entered
   const regex = /^\d{5}(?:[-\s]\d{4})?$/;
   const queryParam = location.match(regex) ? "zip" : "city";
   let currentWeather = "";
@@ -31,7 +34,7 @@ function App() {
     4: "Poor",
     5: "Very Poor",
   };
-  // console.log(pollutionMapping);
+
   const onChange = (event) => {
     setLocation(event.target.value);
   };
@@ -46,47 +49,61 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!location) {
-      return;
-    }
-
-    const APIKey = "7841aaead8245e0fde0256620686a3e1";
 
     if (queryParam === "zip") {
-      currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${location},${countryCode}&units=imperial&appid=${APIKey}`;
+      currentWeather = `https://api.openweathermap.org/data/2.5/weather?zip=${location},${countryCode}&units=imperial&appid=${APIKey}`;
     } else {
       currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${location},${stateCode},${countryCode}&units=imperial&appid=${APIKey}`;
     }
+    // fetch current weather
     fetch(currentWeather)
       .then((response) => {
+        if (response.status !== 200) {
+          setErrorMsg(
+            <div className="errorMsg">
+              Please enter a valid US ZIP code or city, state country code
+              combination
+            </div>
+          );
+        } else {
+          setErrorMsg("");
+        }
         return response.json();
       })
       .then((data) => {
-        setLat(data.coord.lat);
-        setLon(data.coord.lon);
-        setLocationName(data.name);
+        // setLat(data.coord.lat);
+        // setLon(data.coord.lon);
+        setCityName(data.name);
         setCurrentTemp(Math.round(data.main.temp));
         setHumidity(data.main.humidity);
         setWind(Math.round(data.wind.speed));
         setFeelsLike(Math.round(data.main.feels_like));
         setWeather(data.weather[0].main);
         setDescription(data.weather[0].description);
+
+        // fetch air pollution
         fetch(
-          `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${APIKey}`
+          `http://api.openweathermap.org/data/2.5/air_pollution?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIKey}`
         )
           .then((res) => res.json())
           .then((data) => {
-            console.log(data.list[0].main.aqi);
             setPollutionAQI(data.list[0].main.aqi);
           });
+
+        // fetch 5-day forecast
         fetch(
-          `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`
+          `http://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${APIKey}`
+        ).then((res) => res.json());
+
+        // reverse geolocation to get full state and full country names
+        fetch(
+          `http://api.openweathermap.org/geo/1.0/reverse?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIKey}`
         )
           .then((res) => res.json())
           .then((data) => {
-            // console.log(data);
+            setStateName(data[0].state);
+            setCountryName(data[0].country);
           });
-        // });
       });
   };
 
@@ -108,20 +125,40 @@ function App() {
     }
   };
 
+  const outputLocation = () => {
+    return cityName && countryName
+      ? cityName + ", " + stateName + ", " + countryName
+      : errorMsg;
+  };
+
   const displayCurrentWeather = () => {
-    if (currentTemp) {
+    if (currentTemp && cityName && countryName) {
       return (
         <>
           <div className="currentTemperature">
-            <div>{weather}</div>
-            <div>{description}</div>
-            <div>Current Temperature: {currentTemp}&deg;</div>
-            <div>Humidity: {humidity}%</div>
-            <div>Winds: {wind} mph</div>
-            <div>RealFeel Temperature: {feelsLike}&deg;</div>
-          </div>
-          <div className="aqi">
-            Air Quality Index: {pollutionMapping[pollutionAQI]}
+            <div className="centered">
+              <span className="bolded">
+                {weather}- {description}
+              </span>
+            </div>
+            <div>
+              <span className="bolded">Current Temperature:</span> {currentTemp}
+              &deg;
+            </div>
+            <div>
+              <span className="bolded">RealFeel Temperature:</span> {feelsLike}
+              &deg;
+            </div>
+            <div>
+              <span className="bolded">Humidity:</span> {humidity}%
+            </div>
+            <div>
+              <span className="bolded">Winds:</span> {wind} mph
+            </div>
+            <div>
+              <span className="bolded">Air Quality Rating:</span>{" "}
+              {pollutionMapping[pollutionAQI]}
+            </div>
           </div>
         </>
       );
@@ -137,11 +174,12 @@ function App() {
   return (
     <>
       <div className="weatherForm">
-        <h1>how's the weather out there?</h1>
+        {/* <h1>how's the weather out there?</h1> */}
+        <h1>HOW'S THE WEATHER OUT THERE?</h1>
         <div className="formGroup">
           <form onSubmit={handleSubmit} className="getLocation">
             <label>
-              Enter ZIP code or 'city name and state', and country name
+              Enter a US ZIP code or city, state, and country name
               <input
                 value={location}
                 onChange={onChange}
@@ -150,7 +188,6 @@ function App() {
                 name="location"
               />
             </label>
-            {showStateSelect()}
             <select
               onChange={onCountryChange}
               name="selectCountry"
@@ -164,6 +201,7 @@ function App() {
                 );
               })}
             </select>
+            {showStateSelect()}
             <input
               type="submit"
               className="submitLocation"
@@ -172,9 +210,7 @@ function App() {
           </form>
         </div>
       </div>
-      <div className="locationName">
-        {locationName}, {stateName}, {countryCode}
-      </div>
+      <div className="cityName">{outputLocation()}</div>
       <div className="weatherDetails">
         {displayCurrentWeather()}
         {fiveDayForecast()}
@@ -186,19 +222,21 @@ export default App;
 
 // create input field for zip, country/country ----
 // event.target.location.value ----
-// get lat and lon using geolocation API
+// get lat and lon using geolocation API ----
 // dropdown of country codes ----
-// fetch endpoint with API key, and zip code/country passed in
+// fetch endpoint with API key, and zip code/country passed in ----
 // currentWeather.main.temp rounded up to nearest whole number ----
-// fetch pollution endpoint using lat and lon
+// fetch pollution endpoint using lat and lon ----
 // fetch 5 - day forecast using lat and lon
 // validate info they input
 // (dropdown for Celsius vs Fahrenheit)
 // fill out README
-// get rid of spaces in location input field,
-// use either zip or q in api call
-// get country code from entry
+// use either zip or q in api call ----
+// get country code from entry ----
 // (night sky after sundown- background and font color)
 // error handling
 // Change background image for cloudy/rain/snow/clear/nighttime
-// show state dropdown when it's not a zip
+// show state dropdown when it's not a zip ----
+// the commas
+// styling
+// some cities return the wrong results. for example, flushing and forest hills
